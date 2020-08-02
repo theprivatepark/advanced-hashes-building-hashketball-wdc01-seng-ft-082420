3b5cf2ddfc5762c83075cc112f492781b3ec7efb
Anney Park <anneypark@gmail.com>
2020-08-02 22:06:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -63,55 +63,56 @@ def game_hash
     team_name: "Charlotte Hornets",
     colors: ["Turquoise", "Purple"],
     players: [
-    {:player_name
-    :number
-    :shoe
-    :points
-    :rebounds
-    :assists
-    :steals
-    :blocks
-    :slam_dunks},
+      
+    {:player_name => "Jeff Adrien",
+    :number => "4",
+    :shoe => "18",
+    :points => "10",
+    :rebounds => "1",
+    :assists => "1",
+    :steals => "2",
+    :blocks => "7"
+    :slam_dunks => "2"},
     
-    {:player_name
-    :number
-    :shoe
-    :points
-    :rebounds
-    :assists
-    :steals
-    :blocks
-    :slam_dunks}, 
+    {:player_name => "Bismack Biyombo",
+    :number => "0",
+    :shoe => "16",
+    :points => "12",
+    :rebounds => "4",
+    :assists => "7",
+    :steals => "22"
+    :blocks => "15",
+    :slam_dunks => "10"}, 
     
-    {:player_name
-    :number
-    :shoe
-    :points
-    :rebounds
-    :assists
-    :steals
-    :blocks
-    :slam_dunks},
+    {:player_name =>
+    :number =>
+    :shoe =>
+    :points =>
+    :rebounds =>
+    :assists =>
+    :steals =>
+    :blocks =>
+    :slam_dunks => },
     
-    {:player_name
-    :number
-    :shoe
-    :points
-    :rebounds
-    :assists
-    :steals
-    :blocks
-    :slam_dunks},
+    {:player_name =>
+    :number =>
+    :shoe =>
+    :points =>
+    :rebounds =>
+    :assists =>
+    :steals =>
+    :blocks =>
+    :slam_dunks => },
     
-    {:player_name
-    :number
-    :shoe
-    :points
-    :rebounds
-    :assists
-    :steals
-    :blocks
-    :slam_dunks}
+    {:player_name =>
+    :number =>
+    :shoe =>
+    :points =>
+    :rebounds =>
+    :assists =>
+    :steals =>
+    :blocks =>
+    :slam_dunks =>}
 
       ]
   }
