7932035284fe73f843b411f8d707db347c395c75
Anney Park <anneypark@gmail.com>
2020-08-02 22:01:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -63,49 +63,56 @@ def game_hash
     team_name: "Charlotte Hornets",
     colors: ["Turquoise", "Purple"],
     players: [
-{:player_name
-:number
-:shoe
-:points
-:rebounds
-:assists
-:steals
-:blocks
-:slam_dunks},
+    {:player_name
+    :number
+    :shoe
+    :points
+    :rebounds
+    :assists
+    :steals
+    :blocks
+    :slam_dunks},
+    
+    {:player_name
+    :number
+    :shoe
+    :points
+    :rebounds
+    :assists
+    :steals
+    :blocks
+    :slam_dunks}, 
+    
+    {:player_name
+    :number
+    :shoe
+    :points
+    :rebounds
+    :assists
+    :steals
+    :blocks
+    :slam_dunks},
+    
+    {:player_name
+    :number
+    :shoe
+    :points
+    :rebounds
+    :assists
+    :steals
+    :blocks
+    :slam_dunks},
+    
+    {:player_name
+    :number
+    :shoe
+    :points
+    :rebounds
+    :assists
+    :steals
+    :blocks
+    :slam_dunks}
 
-{:player_name
-:number
-:shoe
-:points
-:rebounds
-:assists
-:steals
-:blocks
-:slam_dunks}, {:player_name
-:number
-:shoe
-:points
-:rebounds
-:assists
-:steals
-:blocks
-:slam_dunks}, {:player_name
-:number
-:shoe
-:points
-:rebounds
-:assists
-:steals
-:blocks
-:slam_dunks}, {:player_name
-:number
-:shoe
-:points
-:rebounds
-:assists
-:steals
-:blocks
-:slam_dunks}
       ]
   }
     
