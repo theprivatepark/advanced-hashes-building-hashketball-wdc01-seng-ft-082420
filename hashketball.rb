816234dd16fbbbe861687735077025c0d6e6f905
Anney Park <anneypark@gmail.com>
2020-08-02 22:08:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -104,15 +104,15 @@ def game_hash
     :blocks => "1",
     :slam_dunks => "0"},
     
-    {:player_name =>
-    :number =>
-    :shoe =>
-    :points =>
-    :rebounds =>
-    :assists =>
-    :steals =>
-    :blocks =>
-    :slam_dunks =>}
+    {:player_name => "Kemba Walker",
+    :number => "33",
+    :shoe => "15",
+    :points => "6",
+    :rebounds => "12",
+    :assists => "12",
+    :steals => "7",
+    :blocks => "5",
+    :slam_dunks => "12"}
 
       ]
   }
