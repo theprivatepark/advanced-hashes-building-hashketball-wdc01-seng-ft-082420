3f0eb6d896a63fb1006c829142d926b7e552d2b2
Anney Park <anneypark@gmail.com>
2020-08-02 21:56:08 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -28,7 +28,7 @@ def game_hash
     :blocks => "12",
     :slam_dunks => "7"},
 
-    {:player_name = "Brook Lopez",
+    {:player_name => "Brook Lopez",
     :number => "11",
     :shoe => "17",
     :points => "17",
