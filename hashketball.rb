5875c1f226a753b8c2ac008331109319142e41bf
Anney Park <anneypark@gmail.com>
2020-08-02 22:06:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -84,15 +84,15 @@ def game_hash
     :blocks => "15",
     :slam_dunks => "10"}, 
     
-    {:player_name =>
-    :number =>
-    :shoe =>
-    :points =>
-    :rebounds =>
-    :assists =>
-    :steals =>
-    :blocks =>
-    :slam_dunks => },
+    {:player_name => "DeSagna Diop",
+    :number => "2",
+    :shoe => "14",
+    :points => "24",
+    :rebounds => "12",
+    :assists => "12",
+    :steals => "4",
+    :blocks => "5",
+    :slam_dunks => "5"},
     
     {:player_name =>
     :number =>
